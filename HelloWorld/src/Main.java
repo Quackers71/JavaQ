1e2759e92a74b5d52d748e3b437db5c3d3e8db73
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-08-05 22:01:45 +0100

    working on Desktop

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World Again!");
+	System.out.println("Hello World One");
+	System.out.println("Hello World Two");
+    System.out.println("Hello World Three");
+
     }
 }
