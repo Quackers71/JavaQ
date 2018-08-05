1faa09cabff1f43110789215a5df9a9181e0acfa
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-08-05 22:49:57 +0100

    comments and more comments

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
 
+    /*
+        This is the first line comment
+        This is the second line comment
+     */
+
     public static void main(String[] args) {
-	System.out.println("Hello World One");
-	System.out.println("Hello World Two");
-    System.out.println("Hello World Three");
+    // This is a stand alone comment
+	System.out.println("Hello World One"); // This is a comment
+	//System.out.println("Hello World Two");
+    System.out.println(/*"Hello World Three"*/"This is different text");
 
     }
 }
