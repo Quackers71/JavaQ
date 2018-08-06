15f4462e9186cd5f973a1ec2c5310d78e7d20605
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-08-06 22:47:54 +0100

    Type Conversion

--- Organized/src/com/pluralsight/getorganized/Main.java
+++ Organized/src/com/pluralsight/getorganized/Main.java
@@ -22,5 +22,18 @@ public class Main {
 
         System.out.println(result3);
         System.out.println(result4);
+
+        float floatVal = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 5;
+
+        short result = (short) byteVal;
+        short result0 = (short) (byteVal - longVal);
+        double result10 = longVal - doubleVal;
+        long result11 = (long) (shortVal - longVal + floatVal + doubleVal);
+
+        System.out.println("Success");
     }
 }
