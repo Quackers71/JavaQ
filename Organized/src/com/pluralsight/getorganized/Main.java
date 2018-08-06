906038b0438e04785da7be0ef86078c4dcb0d274
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-08-06 22:32:30 +0100

    Math Order / Operator Precedence

--- Organized/src/com/pluralsight/getorganized/Main.java
+++ Organized/src/com/pluralsight/getorganized/Main.java
@@ -3,6 +3,24 @@ package com.pluralsight.getorganized;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello Get Organized");
-}
+
+        int valA = 21;
+        int valB = 6;
+        int valC = 3;
+        int valD = 1;
+
+        int result1 = valA - valB / valC;
+
+        int result2 = (valA - valB) / valC;
+
+	    System.out.println(result1);
+        System.out.println(result2);
+
+        int result3 = valA / valC * valD + valB;
+
+        int result4 = valA / (valC * (valD + valB));
+
+        System.out.println(result3);
+        System.out.println(result4);
+    }
 }
